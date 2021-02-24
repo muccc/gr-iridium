ba687f63dc421d69ff2f15354d78a63d4c19b4b3
schneider <schneider@blinkenlichts.net>
2021-02-24 20:36:12 +0100

    fix(fft_burst_tagger): Do not introduce an offset in the output stream
    Previously fft_burst_tagger would output a block of (0, 0) samples with
    size `burst_pre_len`. This created an offset in the output stream,
    eventually creating an offset in calculated timestamps.

--- lib/fft_burst_tagger_impl.h
+++ lib/fft_burst_tagger_impl.h
@@ -99,9 +99,12 @@ namespace gr {
 
       uint64_t get_n_tagged_bursts();
 
-      int work(int noutput_items,
-         gr_vector_const_void_star &input_items,
-         gr_vector_void_star &output_items);
+      void forecast(int noutput_items, gr_vector_int& ninput_items_required);
+
+      int general_work(int noutput_items,
+                            gr_vector_int& ninput_items,
+                            gr_vector_const_void_star& input_items,
+                            gr_vector_void_star& output_items);
     };
 
   } // namespace iridium
