94f14cef124c68059c592325ea978cccd8394336
schneider <schneider@blinkenlichts.net>
2021-04-25 01:59:45 +0200

    feat(frame-sorter): Initial C++ implementation

--- lib/frame_sorter_cpp_impl.h
+++ lib/frame_sorter_cpp_impl.h
@@ -13,15 +13,37 @@
 namespace gr {
 namespace iridium {
 
+class frame {
+public:
+    double timestamp;
+    float center_frequency;
+
+    friend bool operator<(const frame& l, const frame& r)
+    {
+        if(std::abs(l.timestamp - r.timestamp) < 1000 &&
+                std::abs(l.center_frequency - r.center_frequency) < 10000) {
+            return false;
+        }
+
+        return std::tie(l.timestamp, l.center_frequency)
+                < std::tie(r.timestamp, r.center_frequency);
+    }
+};
+
+
 class frame_sorter_cpp_impl : public frame_sorter_cpp
 {
 private:
+    std::map<frame, pmt::pmt_t> frames;
 
     void handler(const pmt::pmt_t& msg);
+
 
 public:
     frame_sorter_cpp_impl();
     ~frame_sorter_cpp_impl();
+
+    bool stop();
 
     // Where all the action really happens
     int work(int noutput_items,
