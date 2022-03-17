5290cbb0aa51817845785fa6cf61c1e8c2af1e96
schneider <schneider@blinkenlichts.net>
2022-03-17 01:30:43 +0100

    fft_channelizer: experiment to move burst to pdu into the channelizer

--- lib/fft_channelizer_impl.cc
+++ lib/fft_channelizer_impl.cc
@@ -24,22 +24,22 @@ constexpr bool is_powerof2(int v) {
 
 using input_type = gr_complex;
 using output_type = gr_complex;
-fft_channelizer::sptr fft_channelizer::make(int fft_size, int decimation)
+fft_channelizer::sptr fft_channelizer::make(int fft_size, int decimation, bool activate_streams, int pdu_ports)
 {
-    return gnuradio::make_block_sptr<fft_channelizer_impl>(fft_size, decimation);
+    return gnuradio::make_block_sptr<fft_channelizer_impl>(fft_size, decimation, activate_streams, pdu_ports);
 }
 
 
 /*
  * The private constructor
  */
-fft_channelizer_impl::fft_channelizer_impl(int fft_size, int decimation)
+fft_channelizer_impl::fft_channelizer_impl(int fft_size, int decimation, bool activate_streams, int pdu_ports)
     : gr::sync_decimator("fft_channelizer",
                          gr::io_signature::make(
                              1 /* min inputs */, 1 /* max inputs */, sizeof(input_type)),
-                         gr::io_signature::make(decimation + 1 /* min outputs */,
-                                                decimation + 1 /*max outputs */,
-                                                sizeof(output_type)),
+                         gr::io_signature::make(
+                             activate_streams ? decimation + 1 : 0 /* min outputs */,
+                             activate_streams ? decimation + 1 : 0 /* max outputs */, sizeof(output_type)),
                          decimation),
     d_decimation(decimation),
     d_fft_size(fft_size),
@@ -47,7 +47,13 @@ fft_channelizer_impl::fft_channelizer_impl(int fft_size, int decimation)
     d_fft(fft_size, 1),
     d_ifft(fft_size / decimation, 1),
     d_inverse_overlap(4),
-    d_output_step((fft_size - fft_size / decimation) / decimation)
+    d_output_step((fft_size - fft_size / decimation) / decimation),
+    d_channels(decimation+1),
+    d_channel_active(0),
+    d_pdu_ports(pdu_ports),
+    d_max_burst_size(1000000),
+    d_bursts(decimation+1),
+    d_outstanding(0)
 {
     // FFT size and decimation must be a power of two so the output sizes are
     // also a power of two.
@@ -75,6 +81,16 @@ fft_channelizer_impl::fft_channelizer_impl(int fft_size, int decimation)
     // We want to keep the last 1/d_inverse_overlap block in the history buffer
     set_history(d_fft_size / d_inverse_overlap + 1);
 
+    if(pdu_ports > 0) {
+        for(int i=0; i < pdu_ports; i++) {
+            auto port_name = pmt::mp("cpdus" + std::to_string(i));
+            message_port_register_out(port_name);
+        }
+
+        message_port_register_in(pmt::mp("burst_handled"));
+        set_msg_handler(pmt::mp("burst_handled"), [this](pmt::pmt_t msg) { this->burst_handled(msg); });
+    }
+
     d_out = (fftwf_complex *)volk_malloc(d_fft_size * sizeof(fftwf_complex), volk_get_alignment());
 }
 
@@ -83,6 +99,10 @@ fft_channelizer_impl::fft_channelizer_impl(int fft_size, int decimation)
  */
 fft_channelizer_impl::~fft_channelizer_impl() {}
 
+void fft_channelizer_impl::burst_handled(pmt::pmt_t msg)
+{
+    d_outstanding--;
+}
 
 int fft_shift(int N, int f)
 {
@@ -103,6 +123,88 @@ int negative_offset(int N, int step, int channel, int n)
     return fft_shift(N, channel * step);
 }
 
+float fft_channelizer_impl::channel_center(int channel)
+{
+    float channel_width = 1. / d_decimation;
+    return channel_width / 2 + channel * (float)d_output_step / d_fft_size - 0.5;
+}
+
+float fft_channelizer_impl::channel_lower_border(int channel)
+{
+    float channel_width = 1. / d_decimation;
+    return channel_center(channel) - channel_width / 2;
+}
+
+float fft_channelizer_impl::channel_upper_border(int channel)
+{
+    float channel_width = 1. / d_decimation;
+    return channel_center(channel) + channel_width / 2;
+}
+
+uint64_t fft_channelizer_impl::activated_channels(tag_t &new_burst)
+{
+    uint64_t ret = 0;
+    float relative_frequency = pmt::to_float(pmt::dict_ref(new_burst.value, pmt::mp("relative_frequency"), pmt::PMT_NIL));
+
+    for(int i = 0; i < d_channels; i++) {
+        if(channel_lower_border(i) < relative_frequency && relative_frequency < channel_upper_border(i)) {
+            ret |= (1<<i);
+        }
+    }
+    return ret;
+}
+
+burst_data fft_channelizer_impl::create_burst(tag_t &new_burst, int channel)
+{
+    uint64_t id = pmt::to_uint64(pmt::dict_ref(new_burst.value, pmt::mp("id"), pmt::PMT_NIL));
+    float magnitude = pmt::to_float(pmt::dict_ref(new_burst.value, pmt::mp("magnitude"), pmt::PMT_NIL));
+    double center_frequency = pmt::to_double(pmt::dict_ref(new_burst.value, pmt::mp("center_frequency"), pmt::PMT_NIL));
+    float sample_rate = pmt::to_float(pmt::dict_ref(new_burst.value, pmt::mp("sample_rate"), pmt::PMT_NIL));
+    float relative_frequency = pmt::to_float(pmt::dict_ref(new_burst.value, pmt::mp("relative_frequency"), pmt::PMT_NIL));
+    uint64_t timestamp = pmt::to_uint64(pmt::dict_ref(new_burst.value, pmt::mp("timestamp"), pmt::PMT_NIL));
+    float noise = pmt::to_float(pmt::dict_ref(new_burst.value, pmt::mp("noise"), pmt::PMT_NIL));
+
+    // Adjust the values based on our position behind a potential filter bank
+    center_frequency += channel_center(channel) * sample_rate;
+    sample_rate = sample_rate / d_decimation;
+    relative_frequency = (relative_frequency - channel_center(channel)) * d_decimation;
+
+    burst_data burst = {id, (double)new_burst.offset / d_decimation, magnitude, relative_frequency,
+    center_frequency, sample_rate, timestamp, noise, 0};
+    burst.data = (gr_complex *) malloc(sizeof(gr_complex) * d_max_burst_size);
+    return burst;
+}
+
+void fft_channelizer_impl::append_to_burst(burst_data &burst, const gr_complex * data, size_t n)
+{
+    // If the burst really gets longer than this, we can just throw away the data
+    if(burst.len + n <= d_max_burst_size) {
+        memcpy(burst.data + burst.len, data, n * sizeof(gr_complex));
+        burst.len += n;
+    }
+}
+
+void fft_channelizer_impl::publish_burst(burst_data &burst)
+{
+    pmt::pmt_t d_pdu_meta = pmt::make_dict();
+    pmt::pmt_t d_pdu_vector = pmt::init_c32vector(burst.len, burst.data);
+
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("id"), pmt::mp(burst.id));
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("magnitude"), pmt::mp(burst.magnitude));
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("relative_frequency"), pmt::mp(burst.relative_frequency));
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("center_frequency"), pmt::mp(burst.center_frequency));
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("sample_rate"), pmt::mp(burst.sample_rate));
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("timestamp"), pmt::mp(burst.timestamp));
+    d_pdu_meta = pmt::dict_add(d_pdu_meta, pmt::mp("noise"), pmt::mp(burst.noise));
+
+    pmt::pmt_t msg = pmt::cons(d_pdu_meta,
+            d_pdu_vector);
+
+    d_outstanding++;
+    message_port_pub(pmt::mp("cpdus0"), msg);
+}
+
+
 int fft_channelizer_impl::work(int noutput_items,
                                gr_vector_const_void_star& input_items,
                                gr_vector_void_star& output_items)
@@ -114,6 +216,15 @@ int fft_channelizer_impl::work(int noutput_items,
     // GNURadio supplies us with some history in the front of the buffer. We use
     // this for the overlap of the FFT.
     for(int i = 0; i < ninput_items; i += d_fft_size - d_fft_size / d_inverse_overlap) {
+        std::vector<tag_t> new_bursts;
+
+        // TODO: there are two options here and one is correct...
+        get_tags_in_window(new_bursts, 0, i, i + d_fft_size - d_fft_size / d_inverse_overlap, pmt::mp("new_burst"));
+
+        // No stream outputs, no channel active, no new channel becoming active => No need to generate any output
+        if(output_items.size() == 0 && d_channel_active == 0 && new_bursts.size() == 0) {
+            continue;
+        }
         // Initial forward FFT. No window is applied.
 #if 0
         fftwf_complex * fft_in = (fftwf_complex*)&in[i];
@@ -131,20 +242,67 @@ int fft_channelizer_impl::work(int noutput_items,
         d_fft.execute();
 #endif
 
+
+        for(tag_t new_burst : new_bursts) {
+            d_channel_active |= activated_channels(new_burst);
+        }
+
+        std::vector<tag_t> gone_bursts;
+        get_tags_in_window(gone_bursts, 0, i, i + d_fft_size - d_fft_size / d_inverse_overlap, pmt::mp("gone_burst"));
+
         // Cycle through each output and perform the smaller reverse FFT at the appropriate
         // location of the larger forward FFTs output.
-        for(int j = 0; j < output_items.size(); j++) {
+        for(int j = 0; j < d_channels; j++) {
 
-            // Construct the input to the reverse FFT. We need to copy the two halves
-            // as the output of the initial FFT still is [DC, Positive Freq, Negative Freq].
-            memcpy(&d_ifft.get_inbuf()[0], &d_fft.get_outbuf()[positive_offset(d_fft_size, d_output_step, j, d_ifft_size)], d_ifft_size / 2 * sizeof(gr_complex));
-            memcpy(&d_ifft.get_inbuf()[d_ifft_size / 2], &d_fft.get_outbuf()[negative_offset(d_fft_size, d_output_step, j, d_ifft_size)], d_ifft_size / 2 * sizeof(gr_complex));
+            if(output_items.size() > 0 || d_channel_active & (1 << j)) {
+                // Construct the input to the reverse FFT. We need to copy the two halves
+                // as the output of the initial FFT still is [DC, Positive Freq, Negative Freq].
+                volk_32fc_s32fc_multiply_32fc(&d_ifft.get_inbuf()[0], &d_fft.get_outbuf()[positive_offset(d_fft_size, d_output_step, j, d_ifft_size)], 1./d_fft_size, d_ifft_size / 2);
+                volk_32fc_s32fc_multiply_32fc(&d_ifft.get_inbuf()[d_ifft_size / 2], &d_fft.get_outbuf()[negative_offset(d_fft_size, d_output_step, j, d_ifft_size)], 1./d_fft_size, d_ifft_size / 2);
+                d_ifft.execute();
 
-            d_ifft.execute();
+                if(output_items.size() > 0) {
+                    output_type* out = reinterpret_cast<output_type*>(output_items[j]);
+                    memcpy(&out[i / d_decimation], &d_ifft.get_outbuf()[d_ifft_size / d_inverse_overlap], (d_ifft_size - d_ifft_size / d_inverse_overlap) * sizeof(gr_complex));
+                }
 
-            output_type* out = reinterpret_cast<output_type*>(output_items[j]);
+                if(d_pdu_ports > 0) {
+                    // Fill already active bursts with samples
+                    for(auto& kv : d_bursts[j]) {
+                        append_to_burst(kv.second, &d_ifft.get_outbuf()[d_ifft_size / d_inverse_overlap], (d_ifft_size - d_ifft_size / d_inverse_overlap));
+                    }
 
-            volk_32fc_s32fc_multiply_32fc(&out[i / d_decimation], &d_ifft.get_outbuf()[d_ifft_size / d_inverse_overlap], 1./d_fft_size, (d_ifft_size - d_ifft_size / d_inverse_overlap));
+                    // Create new bursts. As new new bursts might start in the middle of this block,
+                    // the already active bursts get their data before creating the new ones.
+                    for(tag_t new_burst : new_bursts) {
+                        if(activated_channels(new_burst) & (1 << j)) {
+                            burst_data burst = create_burst(new_burst, j);
+                            d_bursts[j][burst.id] = burst;
+                            int max_items = d_ifft_size - d_ifft_size / d_inverse_overlap;
+                            int relative_offset = (new_burst.offset - nitems_read(0) - i) / d_decimation;
+                            int to_copy = max_items - relative_offset;
+                            append_to_burst(d_bursts[j][burst.id], &d_ifft.get_outbuf()[d_ifft_size / d_inverse_overlap + relative_offset], to_copy);
+                        }
+                    }
+
+
+                    // Publish gone bursts
+                    for(tag_t tag : gone_bursts) {
+                        uint64_t id = pmt::to_uint64(pmt::dict_ref(tag.value, pmt::mp("id"), pmt::PMT_NIL));
+
+                        if(d_bursts[j].count(id)) {
+                            burst_data &burst = d_bursts[j][id];
+                            publish_burst(burst);
+                            free(d_bursts[j][id].data);
+                            d_bursts[j].erase(id);
+                        }
+                    }
+
+                    if(d_bursts[j].size() == 0) {
+                        d_channel_active &= ~(1 << j);
+                    }
+                }
+            }
         }
     }
 
--- lib/fft_channelizer_impl.h
+++ lib/fft_channelizer_impl.h
@@ -15,6 +15,20 @@
 namespace gr {
 namespace iridium {
 
+    struct burst_data {
+        uint64_t id;
+        double offset;
+        float magnitude;
+        float relative_frequency;
+        double center_frequency;
+        float sample_rate;
+        uint64_t timestamp;
+        float noise;
+        size_t len;
+        gr_complex * data;
+    };
+
+
 class fft_channelizer_impl : public fft_channelizer
 {
 private:
@@ -23,14 +37,31 @@ private:
       const int d_inverse_overlap;
       const int d_output_step;
       const int d_decimation;
+      const int d_channels;
+      const int d_pdu_ports;
+      const int d_max_burst_size;
+
+      uint64_t d_channel_active;
       fftwf_complex * d_out;
       std::map<fftwf_complex*, fftwf_plan> d_fft_plans;
 
       gr::fft::fft_complex_fwd          d_fft;
       gr::fft::fft_complex_rev          d_ifft;
 
+      float channel_center(int channel);
+      float channel_lower_border(int channel);
+      float channel_upper_border(int channel);
+      uint64_t activated_channels(tag_t &new_burst);
+
+      std::vector<std::map<uint64_t, burst_data>> d_bursts;
+      burst_data create_burst(tag_t &new_burst, int channel);
+      void append_to_burst(burst_data &burst, const gr_complex * data, size_t n);
+      void publish_burst(burst_data &burst);
+
+      int d_outstanding;
+      void burst_handled(pmt::pmt_t msg);
 public:
-    fft_channelizer_impl(int fft_size, int decimation);
+    fft_channelizer_impl(int fft_size, int decimation, bool activate_streams, int pdu_ports);
     ~fft_channelizer_impl();
 
     // Where all the action really happens
