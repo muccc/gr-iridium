dc9ededf6a219836fc1977bc5f962b94d06f7782
schneider <schneider@blinkenlichts.net>
2023-01-13 16:26:15 +0100

    burst-tagger: Print sample number of rx time tag

--- lib/fft_burst_tagger_impl.cc
+++ lib/fft_burst_tagger_impl.cc
@@ -515,7 +515,7 @@ int fft_burst_tagger_impl::work(int noutput_items,
         const double seconds_fraction = pmt::to_double(pmt::tuple_ref(value, 1));
 
         std::cerr << "Got rx_time tag: " << seconds << " + " << seconds_fraction
-                  << " seconds." << std::endl;
+                  << " seconds @ " << d_last_rx_time_offset << " samples." << std::endl;
 
         d_last_rx_time_timestamp =
             seconds * 1000000000ULL + (uint64_t)(seconds_fraction * 1000000000ULL);
